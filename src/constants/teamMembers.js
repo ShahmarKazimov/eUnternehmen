a757df4bd531f46176e1cbd1a08710b2f52e94dd
Shahmar Kazimov <shahmar.kazimoff@gmail.com>
2025-10-13 21:43:30 +0400

    fix: deployment edited

--- src/constants/teamMembers.js
+++ src/constants/teamMembers.js
@@ -1,44 +1,30 @@
+import person1 from '/src/assets/images/person1.jpg';
+import person2 from '/src/assets/images/person2.jpg';
+import person3 from '/src/assets/images/person3.jpg';
+
 export const teamMembers = [
     {
         id: 1,
         name: "Mirulvi Mustafazade",
         position: "Project Developer",
-        image: "/src/assets/images/person2.jpg"
+        image: person2
     },
     {
         id: 2,
         name: "Mirkhagan Mustafazade",
         position: "Managing Director",
-        image: "/src/assets/images/person1.jpg"
+        image: person1
     },
     {
         id: 3,
         name: "Parvis O. Yazdani",
         position: "Prokurist",
-        image: "/src/assets/images/person3.jpg"
+        image: person3
     },
     {
         id: 4,
         name: "Mirulvi Mustafazade",
         position: "Project Developer",
-        image: "/src/assets/images/person2.jpg"
+        image: person2
     },
-    // {
-    //     id: 5,
-    //     name: "Mirkhagan Mustafazade",
-    //     position: "Managing Director",
-    //     image: "/src/assets/images/Contact Modal.png"
-    // },
-    // {
-    //     id: 6,
-    //     name: "Parvis O. Yazdani",
-    //     position: "Prokurist",
-    //     image: "/src/assets/images/storage_chart.png"
-    // },
-    // {
-    //     id: 7,
-    //     name: "Mirulvi Mustafazade",
-    //     position: "Project Developer",
-    //     image: "/src/assets/images/storage_chart.png"
-    // }
 ];
