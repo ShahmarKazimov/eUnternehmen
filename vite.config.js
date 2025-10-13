111bdc3cbef4eb864b6631fb585a33ba831cbb63
Shahmar Kazimov <shahmar.kazimoff@gmail.com>
2025-10-13 17:21:10 +0400

    fix: _

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        imprint: resolve(__dirname, 'imprint.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        about: resolve(__dirname, 'about.html'),
+        terms: resolve(__dirname, 'terms.html'),
+      },
+    },
+  },
 })
